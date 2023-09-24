8c03e0a7bbf5b1a44cd9776d6970c055f6b3b040
Krugman Artem <artem.krugman14@gmail.com>
2023-09-24 14:18:36 +0300

    add homework

---
+++ Homework5/task1&2.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+df = pd.read_csv('sample_data/california_housing_train.csv')
+
+print(df[
+    (df['population'] > 0) &
+    (df['population'] < 500)
+].median_house_value.mean())
+
+min_population = df.population.min()
+print(df[
+    df['population'] == min_population
+].households.max())
+
